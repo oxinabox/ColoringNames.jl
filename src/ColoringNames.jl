886244cb85497edcfc81363525ff4a181ec89557
Lyndon White <oxinabox@ucc.asn.au>
2017-01-26 05:30:29 +0000

    =add input toker

--- src/ColoringNames.jl
+++ src/ColoringNames.jl
@@ -1,5 +1,34 @@
 module ColoringNames
 
-# package code goes here
+using MLDataUtils
+using SwiftObjectStores
+using Iterators
+
+export morpheme_tokenizer
+
+
+"""Higher Order Function,
+return a closure, that can use the rules to tokenize an input.
+"""
+function morpheme_tokenizer(replacement_rules)
+    _cache = Dict() #GOLDPLATE: Type specificity
+    function tokenize(input)
+        get!(_cache, input) do
+            for (pat, sub) in replacement_rules
+                input = replace(input, pat, sub)
+            end
+            split(input)
+        end
+    end
+    tokenize
+end
+
+function morpheme_tokenizer(rule_csv_file::AbstractString)
+    rules = open(rule_csv_file) do fh
+        ObsView(readcsv(fh) , ObsDim.First())
+    end
+    morpheme_tokenizer(rules)
+end;
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,19 @@
 using ColoringNames
 using Base.Test
+using MLDataUtils
 
-# write your own tests here
-@test 1 == 2
+
+@testset "Tokenize" begin
+    rules = ObsView(readcsv(IOBuffer(
+            """blueish,blue ish
+            bluish,blue ish
+            -, - 
+            purplish,purple ish""")) , ObsDim.First())
+    
+    tokenizer = morpheme_tokenizer(rules)
+    
+    @test tokenizer("a b") == ["a", "b"]
+    @test tokenizer("bluish red") == ["blue", "ish","red"]
+    @test tokenizer("red-green") == ["red", "-","green"]
+end
+
