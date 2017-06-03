50fe81ec013d340cce37c9b1b4b183e7d1f31e31
Lyndon White <oxinabox@ucc.asn.au>
2017-06-03 06:12:55 +0000

    =do run the training step

--- expr/run_hyperparam_search_term2col_dist.jl
+++ expr/run_hyperparam_search_term2col_dist.jl
@@ -33,7 +33,6 @@ function main(name, splay_std_dev_in_bins)
     println("initialising $runname network")
     mdl = TermToColorDistributionNetwork(cldata.encoding)
 
-#=
     println("training $runname network")
     extra_data[:training_costs_o] = train!(mdl,
                                         cldata.train.terms_padded,
@@ -43,7 +42,7 @@ function main(name, splay_std_dev_in_bins)
                                         epochs=epochs
                                         )
 
-=#
+
     println("evaluating $runname")
     extra_data[:dev_set_results] = evaluate(mdl, cldata.test.terms_padded, cldata.test.colors)
 
